79a2a0c84e5ddd699f739cbea7811a29ae7b1c68
Aguayoo12 <terrade135@gmail.com>
2022-12-12 14:17:26 +0100

    Segundo commit implantacion del menu

--- src/MenuOperaciones.java
+++ src/MenuOperaciones.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
 
 public class MenuOperaciones {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduce el pirmer numero: ");
+		int num1 = sc.nextInt();
+		System.out.println("Introduce el segundo numero: ");
+		int num2 = sc.nextInt();
+		System.out.println("Introduce: ");
+		System.out.println("1.Para realizar una suma.");
+		int menu = sc.nextInt();
+		
+		switch (menu) {
+		case 1:
+			
+			break;
+		case 2:
+			
+			break;
+			
+		case 3:
+			
+			break;
+		}
 
 	}
 
